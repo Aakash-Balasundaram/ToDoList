7159077e3ecc77366659f9e2946a6e050509216e
Aakash B <aakash735cse@gmail.com>
2025-01-16 18:21:49 +0530

    Initial commit done with adding all the files

--- Documents/My Projects/90_north/90_North_Django/Django/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.UserListView.as_view(), name='user-list'),
-    path('chat/<str:username>/', views.ChatHistoryView.as_view(), name='chat-history'),
-]
